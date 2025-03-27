a905e73e31bd21196b4e0023a72d976ac9bfd86c
Danylo <100484794+Nefriz@users.noreply.github.com>
2025-03-27 21:33:19 +0200

    Update Theme.R

--- Theme.R
+++ Theme.R
@@ -15,8 +15,8 @@ example_data <- data.frame(
   Value = c(10, 15, 20, 25, 30, 5, 10, 15, 20, 25, 8, 12, 18, 22, 28, 3, 7, 11, 16, 21),
   Group = rep(c("X", "Y"), times = 10)
 )
-
-text_size_title = 20
+text_main_title = 29
+text_size_title = 18
 text_size_minor = 14
 palette <- c("#153969", "#2a2e3a", "#718bab", "#f4f4f4", "#ffffff")
 fabulous_theme <- theme(
@@ -24,11 +24,14 @@ fabulous_theme <- theme(
   panel.grid.major.y = element_line(color = palette[3], linewidth = 1),
   panel.grid.minor = element_line(color = alpha(palette[3], 0.3)),
   plot.background = element_rect(fill = alpha(palette[5]), 0.5),
+  plot.title = element_text(color = palette[1], size = text_main_title),
   
   axis.title = element_text(color = palette[1]),
   axis.title.y = element_text(color = palette[1], angle = 0, vjust = 0.5, size = text_size_title),
   axis.text = element_text(color =  palette[1], family = "montserrat", size = text_size_minor),
   axis.title.x = element_text(color = palette[1], family = "montserrat", size = text_size_title),
+  
+  
   
   text = element_text(family = "montserrat"),
   
